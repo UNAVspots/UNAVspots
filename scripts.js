553690e5c217a306ba429d4017e6874a5a0a2b7f
UNAVspots <mnair@alumni.unav.es>
2025-11-18 18:49:00 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,33 +1,16 @@
-{/* <script>
-    document.getElementById("scrollButton").addEventListener("click", function(event) {
-        event.preventDefault(); // prevents page jump
-    document.getElementById("mainpage").scrollIntoView({
-        behavior: "smooth",
-    block: "start"
-    });
-
-    const box = document.querySelector('.box');
-
-    box.addEventListener('click', () => {
-        box.classList.toggle('active');
-    });
-
-</script> */}
-
 <script>
-    // Scroll button
     document.getElementById("scrollButton").addEventListener("click", function(event) {
         event.preventDefault(); // prevents page jump
     document.getElementById("mainpage").scrollIntoView({
         behavior: "smooth",
     block: "start"
-        });
     });
 
-    // Box click grow/shrink
     const box = document.querySelector('.box');
 
     box.addEventListener('click', () => {
         box.classList.toggle('active');
     });
+
 </script>
+
